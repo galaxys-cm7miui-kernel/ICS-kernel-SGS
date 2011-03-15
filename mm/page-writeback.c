b172e69720fcdab04ebc08d12a2e9a1eb7bab545
Matt B <jackdachef@gmail.com>
2011-03-15 16:16:12 +0100

    fix __set_page_dirty_no_writeback() return value (Signed-off-by: Bob Liu)

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -1073,7 +1073,7 @@ EXPORT_SYMBOL(write_one_page);
 int __set_page_dirty_no_writeback(struct page *page)
 {
 	if (!PageDirty(page))
-		SetPageDirty(page);
+		return !TestSetPageDirty(page);
 	return 0;
 }
 
