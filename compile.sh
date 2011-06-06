1078de05dcc4dcf92e5f732723d1db722db6555c
Renaud Allard <renaud@allard.it>
2011-06-06 12:33:51 +0200

    Some slight changes to properly generate image and the mighty logo

--- compile.sh
+++ compile.sh
@@ -5,9 +5,9 @@ echo "building kernel"
 make -j8
 
 echo "creating boot.img"
-../../android/system/device/samsung/aries-common/mkshbootimg.py release/boot.img arch/arm/boot/zImage ../../../out/target/product/galaxysmtd/ramdisk.img ../../../out/target/product/galaxysmtd/ramdisk-recovery.img
+../../android/system/device/samsung/aries-common/mkshbootimg.py release/boot.img arch/arm/boot/zImage ../../android/system/out/target/product/galaxysmtd/ramdisk.img ../../android/system/out/target/product/galaxysmtd/ramdisk-recovery.img
 
 echo "launching packaging script"
 ./release/doit.sh
 
-mv release/CM7*.zip ../../public_html/
+mv release/CM7_FuguMod* ../../public_html/
