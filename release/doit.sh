558b948ef967a0d11d737737574fa87db2815d59
Matt B <jackdachef@gmail.com>
2011-07-12 01:27:35 +0200

    more user-friendly install-script, android-logger enable/disable scripts

--- release/doit.sh
+++ release/doit.sh
@@ -13,13 +13,17 @@ TYPE=$1
 RELVER=$2
 [[ "$RELVER" == '' ]] && RELVER="0"
 
-REL=CM7_${TYPE}_$(date +%Y%m%d-%I)_platypus.zip
+REL=CM7_${TYPE}_$(date +%Y%m%d-%H)_platypus.zip
 
 rm -r release/system 2> /dev/null
 mkdir  -p release/system/lib/modules || exit 1
+cp release/logger.module release/system/lib/modules/logger.ko
 find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 
 cd release && {
+	cp 91logger system/etc/init.d/ || exit 1
+	mkdir -p system/bin
+	cp bin/* system/bin/
 	zip -q -r ${REL} system boot.img META-INF bml_over_mtd bml_over_mtd.sh || exit 1
 	sha256sum ${REL} > ${REL}.sha256sum
 	rm -rf ${TYPE} || exit 1
@@ -27,6 +31,7 @@ cd release && {
 	mv ${REL}* ${TYPE} || exit 1
 } || exit 1
 
-rm system/lib/modules/*
 echo ${REL}
+rm system/lib/modules/*
+rm system/etc/init.d/*
 exit 0
