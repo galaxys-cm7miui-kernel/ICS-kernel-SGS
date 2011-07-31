39c4919703b257ead35cff2a84665505fc88f156
Renaud Allard <renaud@allard.it>
2011-07-31 23:03:30 +0200

    Make setenv more easily tunable and switch compiler to 2011q3

--- setenv.sh
+++ setenv.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 export ARCH=arm 
-export PATH=$PATH:/home/r/arm-2010q1/bin:/usr/java/jdk1.6.0_21/bin
-export CROSS_COMPILE=/home/r/arm-2010q1/bin/arm-none-eabi-
+export TOOLCHAIN=~/arm-2011.03
+export PATH=$PATH:${TOOLCHAIN}/bin:/usr/java/jdk1.6.0_21/bin
+export CROSS_COMPILE=${TOOLCHAIN}/bin/arm-none-eabi-
 #export ac_cv_linux_vers=2.6.32
-export CC=/home/r/arm-2010q1/bin/arm-none-eabi-gcc
-#export CC=/home/r/galaxy/agcc
-export LD=/home/r/arm-2010q1/bin/arm-none-eabi-ld
-export AR=/home/r/arm-2010q1/bin/arm-none-eabi-ar
-export RANLIB=/home/r/arm-2010q1/arm-none-eabi/bin/ranlib
-export PATH=$PATH:/home/r/arm-2010q1/bin
-export PATH=$PATH:/home/r/arm-2010q1/
-export PATH=$PATH:/home/r/arm-2010q1/lib
+export CC=${TOOLCHAIN}/bin/arm-none-eabi-gcc
+export LD=${TOOLCHAIN}/bin/arm-none-eabi-ld
+export AR=${TOOLCHAIN}/bin/arm-none-eabi-ar
+export RANLIB=${TOOLCHAIN}/arm-none-eabi/bin/ranlib
+export PATH=$PATH:${TOOLCHAIN}/bin
+export PATH=$PATH:${TOOLCHAIN}/
+export PATH=$PATH:${TOOLCHAIN}/lib
 export CFLAGS="-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -static -Os -fstack-protector -fstack-protector-all -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64"
 export LDFLAGS="-static -Os"
 export CPPFLAGS="-static -Os"
