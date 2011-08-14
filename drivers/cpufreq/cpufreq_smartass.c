dedeb0ed3604c549601b5421148d6aa0c8773edd
Matt B <jackdachef@gmail.com>
2011-08-15 00:51:45 +0200

    give smartass max 400 MHz during sleep

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -79,7 +79,7 @@ static unsigned int up_min_freq;
 * to minimize wakeup issues.
 * Set sleep_max_freq=0 to disable this behavior.
 */
-#define DEFAULT_SLEEP_MAX_FREQ (200*1000)
+#define DEFAULT_SLEEP_MAX_FREQ (400*1000)
 static unsigned int sleep_max_freq;
 
 /*
