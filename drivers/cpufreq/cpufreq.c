cc9cdf5cecf3089f6a22dfc46cfa5968894cc827
Renaud Allard <renaud@allard.it>
2011-08-23 22:15:30 +0200

    Revert "Import cpufreq from 2.6.36 and add interactive governor to the kernel"
    This reverts commit 1edaf1a8b39271b96f026d39710f6a911c9a0466.
    
    Conflicts:
    
    	.version

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -29,8 +29,6 @@
 #include <linux/completion.h>
 #include <linux/mutex.h>
 
-#include <trace/events/power.h>
-
 #define dprintk(msg...) cpufreq_debug_printk(CPUFREQ_DEBUG_CORE, \
 						"cpufreq-core", msg)
 int exp_UV_mV[6];
@@ -355,9 +353,6 @@ void cpufreq_notify_transition(struct cpufreq_freqs *freqs, unsigned int state)
 
 	case CPUFREQ_POSTCHANGE:
 		adjust_jiffies(CPUFREQ_POSTCHANGE, freqs);
-		dprintk("FREQ: %lu - CPU: %lu", (unsigned long)freqs->new,
-			(unsigned long)freqs->cpu);
-		trace_power_frequency(POWER_PSTATE, freqs->new, freqs->cpu);
 		srcu_notifier_call_chain(&cpufreq_transition_notifier_list,
 				CPUFREQ_POSTCHANGE, freqs);
 		if (likely(policy) && likely(policy->cpu == freqs->cpu))
