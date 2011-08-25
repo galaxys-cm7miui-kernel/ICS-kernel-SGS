a4f7fb5133af38fa7353263ef8aff205c2cec907
Matt B <jackdachef@gmail.com>
2011-08-25 23:11:03 +0200

    add the smoothass governor (by leedroid)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -376,6 +376,9 @@ extern struct cpufreq_governor cpufreq_gov_interactivex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
 extern struct cpufreq_governor cpufreq_gov_smartass;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOTHASS)
+extern struct cpufreq_governor cpufreq_gov_smoothass;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smoothass)
 #endif
 
 
