8d62a1eab944bf9450a16dfc028ace53f6f19316
Matt B <jackdachef@gmail.com>
2011-08-29 22:54:05 +0200

    add ondemandb cpufreq governor (ondemand governor with sampling down factor + stock settings - for longer battery runtime as an alternative to the tweaked ondemand governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -361,6 +361,9 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
 extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDB)
+extern struct cpufreq_governor cpufreq_gov_ondemandb;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandb)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
