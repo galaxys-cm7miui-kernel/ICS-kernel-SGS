24a9f4c24decb0e311f833d507ad59aad693f526
Renaud Allard <renaud@allard.it>
2011-09-01 18:13:11 +0200

    Import mm from 2.6.37

--- include/linux/slab_def.h
+++ include/linux/slab_def.h
@@ -14,7 +14,8 @@
 #include <asm/page.h>		/* kmalloc_sizes.h needs PAGE_SIZE */
 #include <asm/cache.h>		/* kmalloc_sizes.h needs L1_CACHE_BYTES */
 #include <linux/compiler.h>
-#include <linux/kmemtrace.h>
+
+#include <trace/events/kmem.h>
 
 /*
  * Enforce a minimum alignment for the kmalloc caches.
