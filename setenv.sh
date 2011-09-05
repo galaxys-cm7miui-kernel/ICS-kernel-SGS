cbd851fd33bdc1228e706c1ae2f9905e29c91de6
Renaud Allard <renaud@allard.it>
2011-09-05 11:33:03 +0200

    Back to 2009q3

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export ARCH=arm 
-export TOOLCHAIN=~/arm-2010q1
-export TOOLPREFIX=arm-none-eabi
+export TOOLCHAIN=~/arm-2009q3
+export TOOLPREFIX=arm-none-linux-gnueabi
 export PATH=$PATH:${TOOLCHAIN}/bin:/usr/java/jdk1.6.0_21/bin
 export CROSS_COMPILE=${TOOLCHAIN}/bin/${TOOLPREFIX}-
 export CC=${TOOLCHAIN}/bin/${TOOLPREFIX}-gcc
@@ -11,7 +11,8 @@ export RANLIB=${TOOLCHAIN}/arm-eabi/bin/ranlib
 export PATH=$PATH:${TOOLCHAIN}/bin
 export PATH=$PATH:${TOOLCHAIN}/
 export PATH=$PATH:${TOOLCHAIN}/lib
-export CFLAGS="-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -static -Os -fstack-protector -fstack-protector-all -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64 -std=c99"
+#export CFLAGS="-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -static -Os -fstack-protector -fstack-protector-all -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64 -std=c99"
+export CFLAGS="-static -Os -fstack-protector -fstack-protector-all -std=c99"
 export LDFLAGS="-static -Os"
 export CPPFLAGS="-static -Os"
 
