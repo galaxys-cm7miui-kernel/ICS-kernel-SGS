e844333bf58b08a7b65b10509c09e5eb37e3ee01
Renaud Allard <renaud@allard.it>
2011-10-07 09:25:11 +0200

    Remove LPM from the framebuffer

--- drivers/video/samsung/s3cfb.c
+++ drivers/video/samsung/s3cfb.c
@@ -107,9 +107,6 @@ static int s3cfb_draw_logo(struct fb_info *fb)
 		iounmap(logo_virt_buf);
 	}
 #else /*CONFIG_SAMSUNG_GALAXYS*/
-	if (readl(S5P_INFORM5)) //LPM_CHARGING mode
-		memcpy(fb->screen_base, charging, fb->var.yres * fb->fix.line_length);
-	else
 		memcpy(fb->screen_base, LOGO_RGB24, fb->var.yres * fb->fix.line_length);
 #endif
 	return 0;
