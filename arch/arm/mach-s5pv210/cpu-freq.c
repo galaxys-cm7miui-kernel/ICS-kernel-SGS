f099a4b089d6d63eb414e3c457eecad4b6257cf7
Matt B <jackdachef@gmail.com>
2011-10-08 01:29:29 +0200

    add/remove some more stuff

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -95,7 +95,7 @@ struct s5pv210_dvs_conf {
 
 #ifdef CONFIG_DVFS_LIMIT
 static unsigned int g_dvfs_high_lock_token = 0;
-static unsigned int g_dvfs_high_lock_limit = 11; // check that
+static unsigned int g_dvfs_high_lock_limit = 12;
 static unsigned int g_dvfslockval[DVFS_LOCK_TOKEN_NUM];
 //static DEFINE_MUTEX(dvfs_high_lock);
 #endif
