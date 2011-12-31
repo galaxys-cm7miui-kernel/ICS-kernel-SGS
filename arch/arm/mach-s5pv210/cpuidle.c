30caa0657d3f3d380d3143aea6107e55bafe6c6c
Matt B <jackdachef@gmail.com>
2011-12-31 15:32:25 +0100

    enable deepidle by default

--- arch/arm/mach-s5pv210/cpuidle.c
+++ arch/arm/mach-s5pv210/cpuidle.c
@@ -413,7 +413,7 @@ static int s5p_enter_idle_state(struct cpuidle_device *dev,
 	struct timeval before, after;
 	int idle_time;
 #ifdef CONFIG_CPU_DIDLE
-	int idle_state = 0;
+	int idle_state = 1;
 #endif
 
 	local_irq_disable();
